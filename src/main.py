740079a398e71b348e4a7620f20fa2549f4a369b
Renan Gomes <renan_gomes@id.uff.br>
2026-05-10 13:23:55 -0300

    Adiciona música, efeitos do menu e controle e volumes

--- src/main.py
+++ src/main.py
@@ -6,15 +6,37 @@ import gameloop
 class Game():
     def __init__(self):
         pygame.init()
+        pygame.mixer.init()
+        pygame.key.set_repeat(300, 40)
         self.running, self.playing = True, False
-        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
+        self.UP_KEY = False
+        self.DOWN_KEY = False
+        self.LEFT_KEY = False
+        self.RIGHT_KEY = False
+        self.START_KEY = False
+        self.BACK_KEY = False
         self.DISPLAY_W, self.DISPLAY_H = 800, 600
         self.display = pygame.Surface((self.DISPLAY_W, self.DISPLAY_H))
         self.window = pygame.display.set_mode(((self.DISPLAY_W, self.DISPLAY_H)))
         self.font_name = pygame.font.get_default_font()
         self.BLACK, self.WHITE = (0,0,0), (255,255,255)
+
+        self.music_volume = 0.5
+        self.effects_volume = 0.5
+
+        self.cursor_sound = pygame.mixer.Sound("assets/cursor.wav")
+        self.confirm_sound = pygame.mixer.Sound("assets/miado_confirmar.ogg")
+        self.back_sound = pygame.mixer.Sound("assets/miado_voltar.ogg")
+        self.cursor_sound.set_volume(self.effects_volume)
+        self.confirm_sound.set_volume(self.effects_volume)
+
+        pygame.mixer.music.load("assets/catanwaltz.wav")
+        pygame.mixer.music.set_volume(self.music_volume)
+        pygame.mixer.music.play(-1)
+
         self.main_menu = MainMenu(self)
         self.options = OptionsMenu(self)
+        self.volume = VolumeMenu(self)
         self.credits = CreditsMenu(self)
         self.curr_menu = self.main_menu
 
@@ -36,15 +58,37 @@ class Game():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
                     self.START_KEY = True
-                if event.key == pygame.K_BACKSPACE:
+                if event.key == pygame.K_ESCAPE:
                     self.BACK_KEY = True
                 if event.key == pygame.K_DOWN:
                     self.DOWN_KEY = True
                 if event.key == pygame.K_UP:
                     self.UP_KEY = True
+                if event.key == pygame.K_LEFT:
+                    self.LEFT_KEY = True
+                if event.key == pygame.K_RIGHT:
+                    self.RIGHT_KEY = True
 
     def reset_keys(self):
-        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
+        self.UP_KEY = False
+        self.DOWN_KEY = False
+        self.LEFT_KEY = False
+        self.RIGHT_KEY = False
+        self.START_KEY = False
+        self.BACK_KEY = False
+    
+    def play_cursor_sound(self):
+        self.cursor_sound.set_volume(self.effects_volume)
+        self.cursor_sound.play()
+
+
+    def play_confirm_sound(self):
+        self.confirm_sound.set_volume(self.effects_volume)
+        self.confirm_sound.play()
+
+    def play_back_sound(self):
+        self.back_sound.set_volume(self.effects_volume)
+        self.back_sound.play()
 
     def draw_text(self, text, size, x, y):
         font = pygame.font.Font(self.font_name, size)
--- src/menu2.py
+++ src/menu2.py
@@ -11,6 +11,12 @@ class Menu():
     def draw_cursor(self):
         self.game.draw_text("*", 15, self.cursor_rect.x, self.cursor_rect.y)
 
+    def play_cursor_sound(self):
+        self.game.play_cursor_sound()
+
+    def play_confirm_sound(self):
+        self.game.play_confirm_sound()
+
     def blit_screen(self):
         self.game.window.blit(self.game.display,(0,0))
         pygame.display.update()
@@ -40,6 +46,7 @@ class MainMenu(Menu):
 
     def move_cursor(self):
         if self.game.DOWN_KEY:
+            self.game.play_cursor_sound()
             if self.state == 'Start':
                 self.cursor_rect.midtop = (self.optionsx + self.offset, self.optionsy)
                 self.state = 'Options'
@@ -50,6 +57,7 @@ class MainMenu(Menu):
                 self.cursor_rect.midtop = (self.startx + self.offset, self.starty)
                 self.state = 'Start'
         elif self.game.UP_KEY:
+            self.game.play_cursor_sound()
             if self.state == 'Start':
                 self.cursor_rect.midtop = (self.creditsx + self.offset, self.creditsy)
                 self.state = 'Credits'
@@ -63,6 +71,7 @@ class MainMenu(Menu):
     def check_input(self):
         self.move_cursor()
         if self.game.START_KEY:
+            self.game.play_confirm_sound()
             if self.state == 'Start':
                 self.game.playing = True
             elif self.state == 'Options':
@@ -93,9 +102,11 @@ class OptionsMenu(Menu):
     
     def check_input(self):
         if self.game.BACK_KEY:
+            self.game.play_back_sound()
             self.game.curr_menu = self.game.main_menu
             self.run_display = False
         elif self.game.UP_KEY or self.game.DOWN_KEY:
+            self.play_cursor_sound()
             if self.state == 'Volume':
                 self.state = 'Controls'
                 self.cursor_rect.midtop = (self.controlsx + self.offset, self.controlsy)
@@ -103,8 +114,88 @@ class OptionsMenu(Menu):
                 self.state = 'Volume'
                 self.cursor_rect.midtop = (self.volx + self.offset, self.voly)
         elif self.game.START_KEY:
-            # criar a opção dos volumes e controles
-            pass
+            self.play_confirm_sound()
+            if self.state == 'Volume':
+                self.game.curr_menu = self.game.volume
+            elif self.state == 'Controls':
+                pass
+            self.run_display = False
+        
+class VolumeMenu(Menu):
+    def __init__(self, game):
+        Menu.__init__(self, game)
+
+        self.state = 'Music'
+
+        self.musicx, self.musicy = self.mid_w, self.mid_h + 20
+        self.effectsx, self.effectsy = self.mid_w, self.mid_h + 45
+
+        self.cursor_rect.midtop = (self.musicx + self.offset, self.musicy)
+
+    def display_menu(self):
+        self.run_display = True
+
+        while self.run_display:
+            self.game.check_events()
+            self.check_input()
+
+            self.game.display.fill(self.game.BLACK)
+
+            music_percent = round(self.game.music_volume * 100)
+            effects_percent = round(self.game.effects_volume * 100)
+
+            self.game.draw_text("Volume", 20, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 - 40)
+            self.game.draw_text(f"Música: {music_percent}%", 15, self.musicx, self.musicy)
+            self.game.draw_text(f"Efeitos: {effects_percent}%", 15, self.effectsx, self.effectsy)
+            self.game.draw_text("Use as setas para alterar", 12, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 + 100)
+            self.game.draw_text("ESC para voltar", 12, self.game.DISPLAY_W / 2, self.game.DISPLAY_H / 2 + 120)
+
+            self.draw_cursor()
+            self.blit_screen()
+
+    def check_input(self):
+        if self.game.BACK_KEY:
+            self.game.play_back_sound()
+            self.game.curr_menu = self.game.options
+            self.run_display = False
+
+        elif self.game.UP_KEY or self.game.DOWN_KEY:
+            self.play_cursor_sound()
+            if self.state == 'Music':
+                self.state = 'Effects'
+                self.cursor_rect.midtop = (self.effectsx + self.offset, self.effectsy)
+
+            elif self.state == 'Effects':
+                self.state = 'Music'
+                self.cursor_rect.midtop = (self.musicx + self.offset, self.musicy)
+
+        elif self.game.LEFT_KEY:
+            if self.state == 'Music':
+                self.game.music_volume = round(self.game.music_volume - 0.01, 2)
+
+                if self.game.music_volume < 0:
+                    self.game.music_volume = 0
+
+                pygame.mixer.music.set_volume(self.game.music_volume)
+            elif self.state == 'Effects':
+                self.game.effects_volume = round(self.game.effects_volume - 0.01, 2)
+
+                if self.game.effects_volume < 0:
+                    self.game.effects_volume = 0
+
+        elif self.game.RIGHT_KEY:
+            if self.state == 'Music':
+                self.game.music_volume = round(self.game.music_volume + 0.01, 2)
+
+                if self.game.music_volume > 1:
+                    self.game.music_volume = 1
+
+                pygame.mixer.music.set_volume(self.game.music_volume)
+            elif self.state == 'Effects':
+                self.game.effects_volume = round(self.game.effects_volume + 0.01, 2)
+
+                if self.game.effects_volume > 1:
+                    self.game.effects_volume = 1
 
 class CreditsMenu(Menu):
     def __init__(self, game):
@@ -115,6 +206,7 @@ class CreditsMenu(Menu):
         while self.run_display:
             self.game.check_events()
             if self.game.START_KEY or self.game.BACK_KEY:
+                self.game.play_back_sound()
                 self.game.curr_menu = self.game.main_menu
                 self.run_display = False
             self.game.display.fill(self.game.BLACK)
